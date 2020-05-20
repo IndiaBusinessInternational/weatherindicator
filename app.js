789e3fdf9c36c1e639138c7504c9b5e459860cb2
India Business International <54680947+IndiaBusinessInternational@users.noreply.github.com>
2020-05-20 20:38:23 +0530

    add webfiles

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// my api key : f1504a5c4ad4cb46865774508f108b12
+// Tutorial by http://youtube.com/CodeExplained
+// api key : 82005d27a116c2880c8f0fcb866998a0
 
 // SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
@@ -31,7 +32,7 @@ if('geolocation' in navigator){
 function setPosition(position){
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-
+    
     getWeather(latitude, longitude);
 }
 
@@ -44,7 +45,7 @@ function showError(error){
 // GET WEATHER FROM API PROVIDER
 function getWeather(latitude, longitude){
     let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-
+    
     fetch(api)
         .then(function(response){
             let data = response.json();
@@ -78,11 +79,11 @@ function celsiusToFahrenheit(temperature){
 // WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
 tempElement.addEventListener("click", function(){
     if(weather.temperature.value === undefined) return;
-
+    
     if(weather.temperature.unit == "celsius"){
         let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
         fahrenheit = Math.floor(fahrenheit);
-
+        
         tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
         weather.temperature.unit = "fahrenheit";
     }else{
@@ -90,3 +91,4 @@ tempElement.addEventListener("click", function(){
         weather.temperature.unit = "celsius"
     }
 });
+
